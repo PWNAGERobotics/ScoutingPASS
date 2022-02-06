5d8d48135119e32ef5423ea549e110356320df59
mayabose <98671325+mayabose@users.noreply.github.com>
2022-02-05 18:05:02 -0600

    Update scoutingPASS.js
    Added undo button but have not defined the function yet

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -51,7 +51,7 @@ function addCounter(table, idx, name, data){
   var button2 = document.createElement("button");
   button2.setAttribute("type", "checkbox");
   button2.setAttribute("onclick", "counter(this.parentElement, 1)");
-  button2.innerHTML += "+"
+  button2.innerHTML += "+";
   cell2.appendChild(button2);
   return idx+1;
 }
@@ -63,6 +63,17 @@ function addFieldImage(table, idx, name, data) {
   cell.setAttribute("colspan", 2);
   cell.setAttribute("style", "text-align: center;");
   cell.innerHTML = name;
+	
+  row = table.insertRow(idx); 
+  idx += 1;
+  cell = row.insertCell(0);
+  cell.setAttribute("colspan", 2);
+  cell.setAttribute("style", "text-align: center;");
+  var undoButton = document.createElement("button");
+  undoButton.setAttribute("type", "checkbox");
+  undoButton.setAttribute("onclick", "undo(this.parentElement)");
+  undoButton.innerHTML += "Undo";
+  cell.appendChild(undoButton);
 
   row = table.insertRow(idx);
   idx += 1;
@@ -803,6 +814,12 @@ function counter(element, step)
 				ctr.value = 0;
 		}
 }
+
+function undo(element)
+{
+    var fi = element.getElementsByClassName("counter")[0];
+
+		
 
 window.onload = function(){
 	configure();
