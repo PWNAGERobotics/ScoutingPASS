0d6add34d438fa8af8e6e79890eeed281a88c94c
sviera2198 <97629212+sviera2198@users.noreply.github.com>
2022-02-15 19:45:30 -0500

    Update Competition for week 0

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022NHGRS",
+        "defaultValue":"2022WK0",
         "required":"true",
         "disabled":"true"
       },
