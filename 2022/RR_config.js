0fca5680290ac342da5a2e23e7a6dee80f8b1a2c
sviera2198 <97629212+sviera2198@users.noreply.github.com>
2022-03-03 12:59:36 -0500

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022WK0",
+        "defaultValue":"2022NHGRS",
         "required":"true",
         "disabled":"true"
       },
