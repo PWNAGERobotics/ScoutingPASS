f0abba212ea1bfd49a05f761447df67712623128
Darren Hagler <dphagler@gmail.com>
2022-03-06 20:54:19 -0500

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022gadal",
+        "defaultValue":"2022txwac",
         "required":"true",
         "disabled":"true"
       },
