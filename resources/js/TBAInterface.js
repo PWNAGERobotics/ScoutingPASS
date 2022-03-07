918ff0f1c4f09b9d31bd81e6965c7b30e789f096
Darren Hagler <dphagler@gmail.com>
2022-03-06 20:40:30 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "P1OCFTEclkeAJO4vxlwirA4OqmrDyE7wqBLqTZTXftp2iXzg10KLFgQJ8aS7sFsQ";
 
 /**
  * Get list of teams in event
