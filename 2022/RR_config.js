fd6ac4db58d88e8bf85e64c8d6c035734ef8d0cf
Azh86998 <98558703+Azh86998@users.noreply.github.com>
2022-03-23 13:57:08 -0400

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022gacol",
+        "defaultValue":"2022gaalb",
         "required":"true",
         "disabled":"true"
       },
