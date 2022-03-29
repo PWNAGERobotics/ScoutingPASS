852910ab57f3b327b14ed662c56b7de6806aef58
mayabose <98671325+mayabose@users.noreply.github.com>
2022-03-29 15:13:32 -0500

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022ilpe",
+        "defaultValue":"2022wila",
         "required":"true",
         "disabled":"true"
       },
