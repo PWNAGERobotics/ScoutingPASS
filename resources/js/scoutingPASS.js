2796102f1d44283d05af41c868324ba48ac9838d
fuddster <fuddster@users.noreply.github.com>
2022-12-04 19:46:24 -0600

    Fix flip button formatting

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -104,7 +104,7 @@ function addFieldImage(table, idx, name, data) {
   flipButton.innerHTML += "Flip Image";
   flipButton.setAttribute("id", "flip_"+data.code);
   flipButton.setAttribute("class", "flipButton");
-  flipButton.setAttribute("margin-right", '8px');
+  flipButton.setAttribute("margin-left", '8px');
   cell.appendChild(flipButton);
 
   row = table.insertRow(idx);
