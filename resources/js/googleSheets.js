9079777ea977bb907505fe1a4949cc3467b22027
fuddster <fuddster@users.noreply.github.com>
2022-12-31 22:03:31 -0500

    Create googleSheets.js

---
+++ resources/js/googleSheets.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("#scoutingForm")
+const submitButton = document.querySelector("#submit")
+const scriptURL = '<INSERT SCRIPT URL>'
+
+form.addEventListener('submit', e => {
+  submitButton.disabled = true
+  e.preventDefault()
+  let requestBody = new FormData(form)
+  fetch(scriptURL, { method: 'POST', body: requestBody})
+    .then(response => {
+      alert('Success!', response)
+      submitButton.disabled = false
+    })
+    .catch(error => {
+      alert('Error!', error.message)
+      submitButton.disabled = false
+    })
+})
