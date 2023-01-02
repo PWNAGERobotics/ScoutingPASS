c6bb7ce38c877a53a3c8f6a15c035adf7c9043dd
fuddster <fuddster@users.noreply.github.com>
2023-01-01 22:31:37 -0500

    Update googleSheets.js

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -14,14 +14,14 @@ function setUpGoogleSheets() {
       console.log(form)
       fetch(scriptURL, { method: 'POST', mode: 'no-cors', body: new FormData(form) })
         .then(response => { 
-              btn.disabled = false
-              btn.innerHTML = "Submit"
               alert('Success!', response) })
         .catch(error => {
-              btn.disabled = false
-              btn.innerHTML = "Submit"
               alert('Error!', error.message)})
     })
+
+    btn.disabled = false
+    btn.innerHTML = "Send to Google Sheets"
+
 //   const form = document.querySelector("#scoutingForm")
 //   const submitButton = document.querySelector("#submit")
 //   const scriptURL = 'https://script.google.com/macros/s/AKfycbxSyy853BpmXoKLv2VOVmS8PJ4yqC_XIuQI-aeFFXPRxAuCmEbaF5N27uu-hcPweMC-/exec'
