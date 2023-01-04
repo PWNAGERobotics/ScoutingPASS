0be7ce244be7bdaf8e775fa9fa7d4c9c7cab06ef
fuddster <fuddster@users.noreply.github.com>
2023-01-03 22:29:11 -0500

    Update googleSheets.js

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -3,18 +3,18 @@ function setUpGoogleSheets() {
     const form = document.querySelector('#scoutingForm')
     const btn = document.querySelector('#submit')
  
+    
     form.addEventListener('submit', e => {
       e.preventDefault()
       btn.disabled = true
-      btn.innerHTML = "Loading..."
+      btn.innerHTML = "Sending..."
 
-      console.log(form)
-      let fd = new FormData(form)
+      let fd = getData(false)
       for (const [key, value] of fd) {
         console.log(`${key}: ${value}\n`);
       }
 
-      fetch(scriptURL, { method: 'POST', mode: 'no-cors', body: new FormData(form) })
+      fetch(scriptURL, { method: 'POST', mode: 'no-cors', body: fd })
         .then(response => { 
               alert('Success!', response) })
         .catch(error => {
@@ -24,4 +24,3 @@ function setUpGoogleSheets() {
       btn.innerHTML = "Send to Google Sheets"
     })
 }
-
