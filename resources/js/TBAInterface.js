3bbeff15fe5f5e8edc27e51a9f913188e0942a0b
Justin Sticht <42072776+justicht@users.noreply.github.com>
2023-02-14 12:15:08 -0600

    Updated Event Code and TBA aPI

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "ccfefc76-fadf-4323-b663-f31373377662";
 /**
  * Get list of teams in event
  *
