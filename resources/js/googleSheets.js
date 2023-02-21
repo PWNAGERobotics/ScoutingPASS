95893871a49090e4cfd2e9e8cf2f37ae2210b2d2
rlr244 <102827721+rlr244@users.noreply.github.com>
2023-02-21 14:42:38 -0500

    Update googleSheets.js

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -1,5 +1,5 @@
 function setUpGoogleSheets() {
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbw0Islpelxz5iqIZ167vrxvUunr43KqQYNQ_R09FaYGjzWeGu_5XZBi3TXodBoKQSGw/exec'
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbwsYrJ_uo2FlAC_Kbi9Gi2Qoec59j4NOhTD7GRHPVLBH-3HRZXN6wcp1B9SBEZscgbz/exec'
     const form = document.querySelector('#scoutingForm')
     const btn = document.querySelector('#submit')
  
