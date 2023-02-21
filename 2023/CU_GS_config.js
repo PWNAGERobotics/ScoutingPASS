596154032bf38ab68ee30eacfba8372cb65508ab
rlr244 <102827721+rlr244@users.noreply.github.com>
2023-02-21 14:43:35 -0500

    Update CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2022carv",
+      "defaultValue": "GAALB",
       "required": "true",
       "disabled": "true"
     },
