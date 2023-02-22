1aa6b8899582c379b1ef4deb4469c08547368f57
rlr244 <102827721+rlr244@users.noreply.github.com>
2023-02-22 09:23:13 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "gsCol": "e",
       "code": "e",
       "type": "event",
-      "defaultValue": "Wednesday test",
+      "defaultValue": "Wednesday test2",
       "required": "true",
       "disabled": "true"
     },
