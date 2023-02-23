7069c634b2900e2100f3d9417e554636585c005d
lstone24 <lostonel22@stu.lok12.org>
2023-02-22 20:47:50 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -183,6 +183,12 @@ var config_data = `
       },
       "defaultValue": "x"
     },
+	{ "name": "Who did they play defense against?",
+      "code": "wda",
+      "type": "text",
+      "size": 15,
+      "maxSize": 100
+    }
     { "name": "Swerve drive?",
       "code": "sd",
       "type": "bool"
