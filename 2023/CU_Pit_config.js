d8a77b1f1aab7845db2cdad77969e1300f7301c4
lstone24 <lostonel22@stu.lok12.org>
2023-02-22 22:09:50 -0500

    Update CU_Pit_config.js
    added the gear ratios and speed for swerve

--- 2023/CU_Pit_config.js
+++ 2023/CU_Pit_config.js
@@ -39,10 +39,9 @@ var config_data = `
       "code": "sr",
       "type": "radio",
       "choices": {
-        "1": "L1<br>",
-        "2": "L2<br>",
-        "3": "L3<br>",
-        "4": "L4<br>",
+        "1": "L1 (8.14:1 or 12-13 ft/sec)<br>",
+        "2": "L2 (6.75:1 or 14-16 ft/sec)<br>",
+        "3": "L3 (6.12:1 or 16-18 ft/sec)<br>",
         "x": "Not Swerve"
       },
       "defaultValue":"x"
