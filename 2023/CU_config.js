370acf1fa986918acd0e4a2dde737477e71b396a
rlr244 <102827721+rlr244@users.noreply.github.com>
2023-02-28 12:18:38 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -16,7 +16,7 @@ var config_data = `
       "gsCol": "e",
       "code": "e",
       "type": "event",
-      "defaultValue": "Becca",
+      "defaultValue": "2020ilch",
       "required": "true",
       "disabled": "true"
     },
