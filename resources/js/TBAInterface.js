525ffd0cdec3faaf30ef8d39b8a5f5ca131a1d0e
rlr244 <102827721+rlr244@users.noreply.github.com>
2023-02-28 11:37:10 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "YG2SQGxEkWwmGp0lpZ3hx7CgeuuoP5rJQ5AOlJS4J5J36NRB62JWrfYFIRFWYK2i";
 /**
  * Get list of teams in event
  *
