dea630d62486075ddc7a6258068611c97ad4da84
rlr244 <102827721+rlr244@users.noreply.github.com>
2023-02-28 11:31:33 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -10,7 +10,7 @@ var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6"
 function getTeams(ISDE1) {
 	if (authKey) {
 		var xmlhttp = new XMLHttpRequest();
-		var url = "https://www.thebluealliance.com/api/v3/event/" + eventCode + "/teams/simple";
+		var url = "https://www.thebluealliance.com/api/v3/event/" + ISDE1 + "/teams/simple";
 		xmlhttp.open("GET", url, true);
 		xmlhttp.setRequestHeader("X-TBA-Auth-Key", authKey);
 		xmlhttp.onreadystatechange = function() {
@@ -32,7 +32,7 @@ function getTeams(ISDE1) {
 function getSchedule(ISDE1) {
 	if (authKey) {
 		var xmlhttp = new XMLHttpRequest();
-		var url = "https://www.thebluealliance.com/api/v3/event/" + eventCode + "/matches/simple";
+		var url = "https://www.thebluealliance.com/api/v3/event/" + ISDE1 + "/matches/simple";
 		xmlhttp.open("GET", url, true);
 		xmlhttp.setRequestHeader("X-TBA-Auth-Key", authKey);
 		xmlhttp.onreadystatechange = function() {
