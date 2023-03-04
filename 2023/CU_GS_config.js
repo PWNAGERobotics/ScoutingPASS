3a962020842eb1d70266c1b04bd7d877a79466ba
Tom Weissinger <tweissin@gmail.com>
2023-03-04 07:27:37 -0500

    Update CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023week0",
+      "defaultValue": "2023mabri",
       "required": "true",
       "disabled": "true"
     },
