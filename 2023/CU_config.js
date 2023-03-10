aa5381a1175ed7cc193b637d3b2b5f8b28889031
fuddster <fuddster@users.noreply.github.com>
2023-03-10 11:28:50 -0600

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2022carv",
+      "defaultValue": "2023ilch",
       "required": "true",
       "disabled": "true"
     },
