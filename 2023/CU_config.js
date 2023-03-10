2cab3aeb27f33299076d82126165510eb3830654
lstone24 <lostonel22@stu.lok12.org>
2023-03-10 11:08:26 -0500

    Update CU_config.js
    changed from jackson event to stanish event

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023mijac",
+      "defaultValue": "2023mista",
       "required": "true",
       "disabled": "true"
     },
