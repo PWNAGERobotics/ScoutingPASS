86a515b02315e78466d87a2d6d2e1d1440c479ae
ishaqasad <99292326+ishaqasad@users.noreply.github.com>
2023-03-17 12:59:50 -0400

    Update CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023humb",
+      "defaultValue": "2023carv",
       "required": "true",
       "disabled": "true"
     },
