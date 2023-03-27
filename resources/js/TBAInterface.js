a4711e9041e3456805029e43bed383e92bb9743e
iTreffle <129120575+iTreffle@users.noreply.github.com>
2023-03-27 19:31:46 -0400

    Modif auth key

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "8RfFDwp8HNdiIqrIBQ9YHGmZl9Ivr2X1fHcN5qVtvYDZxraP5WXqs2I7AVBo4Q3l";
 /**
  * Get list of teams in event
  *
