99856fd8c57e209fa19a7446b117717574fa98e0
Jason Herzog <herzogjh@gmail.com>
2023-05-05 23:03:41 -0500

    Change to 2023MNST

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023ARC",
+      "defaultValue": "2023MNST",
       "required": "true",
       "disabled": "true"
     },
