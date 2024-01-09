2d032882719ee85211deb432ac9f97093ac306dc
aotlacan <121581398+aotlacan@users.noreply.github.com>
2024-01-09 01:11:54 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023tnkn",
+      "defaultValue": "2023mirr",
       "required": "true"
     },
     { "name": "Match Level",
