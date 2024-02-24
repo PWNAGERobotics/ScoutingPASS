16895fb5f4a9df8ed667836b14fd68cd36cd483c
MorkXII <101903260+MorkXII@users.noreply.github.com>
2024-02-24 08:38:46 -0500

    Update TBAInterface.js
    added TBA api key

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "36BcI3NiqboIgAyCCQ6iFM0K2QJTYxswu0ddHuaL8zfV9WdrelqUcyMlJgZB2FMM";
 /**
  * Get list of teams in event
  *
