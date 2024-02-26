e9024de8df555c2911692aee2f7426a6cbcd35d5
Prashant Kondayapalepu <prashant777k@gmail.com>
2024-02-25 23:05:36 -0800

    color redesign

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024tnkn",
+      "defaultValue": "2023tnkn",
       "required": "true"
     },
     { "name": "Match Level",
