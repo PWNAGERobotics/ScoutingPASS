80a28fd9dbdbffaab94d5d3281fdad64370865fe
MorkXII <101903260+MorkXII@users.noreply.github.com>
2024-02-27 20:47:46 -0500

    Update crescendo_pit_config.js

--- 2024/crescendo_pit_config.js
+++ 2024/crescendo_pit_config.js
@@ -14,11 +14,6 @@ var config_data = `
       "type": "number",
       "defaultValue": "0"
     },
-    { "name": "Weight",
-      "code": "wei",
-      "type": "number",
-      "defaultValue": "0"
-    },
     { "name": "Drivetrain",
       "code": "drv",
       "type": "radio",
@@ -37,36 +32,16 @@ var config_data = `
       "size": 20,
       "maxSize": 50
     },
-    { "name": "Swerve Ratio",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
-        "1": "L1 (8.14:1)<br>",
-        "2": "L2 (6.75:1)<br>",
-        "3": "L3 (6.12:1)<br>",
-        "4": "L4 (5.14:1)<br>",
-        "o": "Other ratio (put in comments)<br>",
-        "x": "Not Swerve"
-      },
-      "defaultValue":"x"
-    },
-    { "name": "Drivetrain Motor",
-      "code": "mot",
-      "type": "radio",
-      "choices": {
-        "n": "Neo<br>",
-        "f": "Falcon<br>",
-        "c": "CIM<br>",
-        "x": "Other<br>"
-      },
-      "defaultValue":"x"
-    },
-    { "name": "# of Batteries",
-      "code": "nob",
-      "type": "number"
-    },
     { "name": "Floor pickup Notes",
       "code": "fpu",
+      "type": "bool"
+    },
+    { "name": "Score Amp in Tele",
+      "code": "amp",
+      "type": "bool"
+    },
+    { "name": "Score Trap",
+      "code": "trp",
       "type": "bool"
     },
     { "name": "Autos",
