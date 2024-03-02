c21c692a6c0ab57604bff91717e99a498f24fa4f
Prashant Kondayapalepu <68829629+prashantkon@users.noreply.github.com>
2024-03-01 23:37:20 -0800

    Dev v2 (#2)
    * Added new auto system specifically an option for the users to map the path during autonomous.
    
    * color redesign to the gold and red colors to increase user visibility and promote team spirit
    
    * Added logo for FRC team
    
    * fixed spelling issues on the document
    
    * Added instructions for autonomous mapping the paths
    
    * Configured SVR day1 match lineups to make it easier for scouters.

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "zdg1CFOM0AojfMxByypNJKk7E0d4POvHsdNClZ9Hj3ov0ryfm46ElFwZ9QeL6JEU";
 /**
  * Get list of teams in event
  *
