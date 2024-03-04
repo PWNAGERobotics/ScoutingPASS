7c82068c57c0cdc6d72fca2dc5e747aaab186631
MorkXII <101903260+MorkXII@users.noreply.github.com>
2024-03-04 16:56:03 -0500

    Update crescendo_pit_config.js

--- 2024/crescendo_pit_config.js
+++ 2024/crescendo_pit_config.js
@@ -2,7 +2,7 @@ var config_data = `
 {
   "dataFormat": "tsv",
   "title": "Scouting PASS 2024",
-  "page_title": "Crescendo",
+  "page_title": "Crescendo - Copper Hawks",
   "pitConfig": "true",
   "prematch": [
     { "name": "Team Number",
