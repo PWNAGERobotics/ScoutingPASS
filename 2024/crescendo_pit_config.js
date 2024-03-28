a12f549153cf220d67426a9c1d9a545c6f626d00
Araav Nayak <104335810+AraavNayak@users.noreply.github.com>
2024-03-27 19:21:01 -0700

    Update crescendo_pit_config.js for MBR

--- 2024/crescendo_pit_config.js
+++ 2024/crescendo_pit_config.js
@@ -9,83 +9,52 @@ var config_data = `
       "code": "t",
       "type": "number"
     },
-    { "name": "Width",
+    { "name": "Scoring capability",
       "code": "wid",
-      "type": "number",
-      "defaultValue": "0"
-    },
-    { "name": "Weight",
-      "code": "wei",
-      "type": "number",
-      "defaultValue": "0"
-    },
-    { "name": "Drivetrain",
-      "code": "drv",
       "type": "radio",
       "choices": {
-        "s": "Swerve<br>",
-        "w": "West Coast/Tank<br>",
-        "b": "Butterfly/Grashopper<br>",
-        "m": "Mechanum<br>",
-        "o": "Other"
-      },
-      "defaultValue": "o"
+        "amp": "Amp<br>",
+        "spk": "Speaker<br>",
+        "amp+spk": "Both<br>"
+      }
     },
-    { "name": "Other Drivetrain",
+    { "name": "Hang from chain?",
+      "code": "wei",
+      "type": "radio",
+      "choices": {
+        "+chain+H": "Yes with harmony<br>",
+        "+chain-H": "Yes but no harmony<br>",
+        "-chain-H": "No<br>"
+      }
+    },
+    { "name": "Avg cycle estimate",
+      "code": "drv",
+      "type": "number"
+    },
+    { "name": "Pickup",
       "code": "odt",
-      "type": "text",
-      "size": 20,
-      "maxSize": 50
+      "type": "radio",
+      "choices": {
+        "g_pickup": "Ground<br>",
+        "s_pickup": "Source<br>",
+        "b_pickup": "Both<br>"
+      }
     },
-    { "name": "Swerve Ratio",
+    { "name": "Can score trap?",
       "code": "sr",
       "type": "radio",
       "choices": {
-        "1": "L1 (8.14:1)<br>",
-        "2": "L2 (6.75:1)<br>",
-        "3": "L3 (6.12:1)<br>",
-        "4": "L4 (5.14:1)<br>",
-        "o": "Other ratio (put in comments)<br>",
-        "x": "Not Swerve"
-      },
-      "defaultValue":"x"
+        "y_trap": "Yes<br>",
+        "n_trap": "No<br>"
+      }
     },
-    { "name": "Drivetrain Motor",
+    { "name": "Auto strat",
       "code": "mot",
-      "type": "radio",
-      "choices": {
-        "n": "Neo<br>",
-        "f": "Falcon<br>",
-        "c": "CIM<br>",
-        "x": "Other<br>"
-      },
-      "defaultValue":"x"
+      "type": "text"
     },
-    { "name": "# of Batteries",
+    { "name": "Additional notes",
       "code": "nob",
-      "type": "number"
-    },
-    { "name": "Floor pickup Notes",
-      "code": "fpu",
-      "type": "bool"
-    },
-    { "name": "Autos",
-      "code": "aut",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
-    },
-    { "name": "Scouting Method /<br>Program (ScoutingPASS?)",
-      "code": "sct",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
-    },
-    { "name": "Comments",
-      "code": "co",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
+      "type": "text"
     }
   ],
   "auton": [
