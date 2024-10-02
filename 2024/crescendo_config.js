605e640d3cccddb678b22bccea3cef3a8beae77a
MorkXII <101903260+MorkXII@users.noreply.github.com>
2024-10-02 14:19:42 -0400

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024oncmp2",
+      "defaultValue": "2024onsc",
       "required": "true"
     },
     { "name": "Match Level",
