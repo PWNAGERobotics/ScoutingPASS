9f083de5e89571eb6ab59622898857d76c407119
Rich Watson <richard.watson0126@gmail.com>
2025-01-09 19:11:01 -0500

    Cleanup

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -798,17 +798,15 @@ function validateData() {
   var errStr = "";
   for (rf of requiredFields) {
     var thisRF = document.forms.scoutingForm[rf];
-	if (typeof myObj !== 'undefined') {
-		if (thisRF.value == "[]" || thisRF.value.length == 0) {
-		  if (rf == "as") {
-			rftitle = "Auto Start Position"
-		  } else {
-			thisInputEl = thisRF instanceof RadioNodeList ? thisRF[0] : thisRF;
-			rftitle = thisInputEl.parentElement.parentElement.children[0].innerHTML.replace("&nbsp;","");
-		  }
-		  errStr += rf + ": " + rftitle + "\n";
-		  ret = false;
-		}
+	if (thisRF.value == "[]" || thisRF.value.length == 0) {
+	  if (rf == "as") {
+		rftitle = "Auto Start Position"
+	  } else {
+		thisInputEl = thisRF instanceof RadioNodeList ? thisRF[0] : thisRF;
+		rftitle = thisInputEl.parentElement.parentElement.children[0].innerHTML.replace("&nbsp;","");
+	  }
+	  errStr += rf + ": " + rftitle + "\n";
+	  ret = false;
 	}
   }
   if (ret == false) {
