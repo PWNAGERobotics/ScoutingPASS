500b1cf3d979ebf1ba34d3c63dfe8b121fe97183
Jacobmyers07 <ogatomicbricks@gmail.com>
2025-03-12 20:45:19 -0400

    Update RS_config.js

--- 2025/RS_config.js
+++ 2025/RS_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025mifer",
+      "defaultValue": "2025MIBKN",
       "required": "true",
       "disabled": "true"
     },
