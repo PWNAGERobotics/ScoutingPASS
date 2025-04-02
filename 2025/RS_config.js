fd49edf294247572ee48ed24deb522dc4a4f9eb8
Jacobmyers07 <ogatomicbricks@gmail.com>
2025-04-02 13:39:37 -0400

    Update RS_config.js

--- 2025/RS_config.js
+++ 2025/RS_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025azgl",
+      "defaultValue": "2025micmp4",
       "required": "true",
       "disabled": "false"
     },
