c868da888c2d1c4bfec1ffd881a039f4ee023474
Jacobmyers07 <ogatomicbricks@gmail.com>
2025-04-17 09:10:38 -0400

    Update RS_config.js
    Tried to get it to auto fill matches by making 2025DALY

--- 2025/RS_config.js
+++ 2025/RS_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025daly",
+      "defaultValue": "2025DALY",
       "required": "true",
       "disabled": "false"
     },
