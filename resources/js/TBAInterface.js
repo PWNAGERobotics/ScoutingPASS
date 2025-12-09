4e5754f00b60ca46c36a23681be5b3f5a570cf6a
Michael M <mmarchiniakjr@hotmail.com>
2025-12-08 20:39:09 -0600

    Update README formatting and remove hardcoded authKey from TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,6 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
 /**
  * Get list of teams in event
  *
