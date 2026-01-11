7d176d3be5064e613b353e26736327d12d306ff3
fuddster <fuddster@users.noreply.github.com>
2026-01-10 22:23:13 -0600

    Update defaultValue for Event to '2026ilch'

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025ilpe",
+      "defaultValue": "2026ilch",
       "required": "true"
     },
     { "name": "Match Level",
